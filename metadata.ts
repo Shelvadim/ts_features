2c0a26b4af23f4d8bd2ebb9948d05fab95eb6599
Shelgachev Vadim <Shelgachev@yahoo.com>
2021-04-20 10:40:44 -0700

    metadata

---
+++ metadata.ts
@@ -0,0 +1,47 @@
+import 'reflect-metadata';
+
+const plane = {
+  color: 'red',
+};
+
+/*Reflect.defineMetadata('note', 'hi there', plane);
+Reflect.defineMetadata('height', 10, plane);
+
+console.log(plane);
+
+const note = Reflect.getMetadata('note', plane);
+const height = Reflect.getMetadata('height', plane);
+
+console.log(note);
+console.log(height);*/
+
+Reflect.defineMetadata('note', 'hi there', plane, 'color');
+const note = Reflect.getMetadata('note', plane, 'color');
+console.log(note);
+console.log(plane);
+
+@printMetadata
+class Plane {
+  color: string = 'red';
+
+  @markFunction('123')
+  fly(): void {
+    console.log('vrrrrr');
+  }
+}
+
+function markFunction(secretInfo: string) {
+  return function (target: Plane, key: string) {
+    Reflect.defineMetadata('secret', secretInfo, target, key);
+  };
+}
+
+function printMetadata(target: typeof Plane) {
+  for (let key in target.prototype) {
+    const secret = Reflect.getMetadata('secret', target.prototype, key);
+    console.log(secret);
+  }
+}
+
+//const secret = Reflect.getMetadata('secret', Plane.prototype, 'fly');
+//console.log(secret);
